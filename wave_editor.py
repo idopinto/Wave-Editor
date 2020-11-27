a0db3806641584d1887e768a482f0fba924944e2
idopinto <ido.pinto@mail.huji.ac.il>
2020-11-27 22:13:31 +0200

    add function 1-7 empty

--- wave_editor.py
+++ wave_editor.py
@@ -1,6 +1,7 @@
-import wave_helper
+import wave_helper, copy
 
-MENU_CHOICES = {1: "Reverse",2: "negative", 3: "accelerate", 4: "slow",5: "volume up", 6:"volume down",7: "filter dim"}
+# MENU_CHOICES = {1: "Reverse",2: "negative", 3: "accelerate", 4: "slow",5: "volume up", 6:"volume down",7: "filter dim"}
+INT_RANGE = (-32768,32767)
 def start_menu():
     # menu:
     # 1. change wave file
@@ -33,21 +34,74 @@ def print_menu():
     print("5.volume up")
     print("6.volume down")
     print("7.filter dim")
+
 def get_file():
     """
 
     :param filename:
     :return:
     """
-    file_name_input = input("enter wav filename: ")
+    file_name_input = "sample1.wav" # input("enter wav filename: ")
     return file_name_input
+
+
+def reverse_audio():
+    pass
+
+
+def negative_audio():
+    pass
+
+
+def accelerate_audio(edited_wave_file_list):
+    pass
+
+
+def slow_down_audio(edited_wave_file_list):
+    pass
+
+
+def volume_up_audio(edited_wave_file_list):
+    pass
+
+
+def volume_down_audio(edited_wave_file_list):
+    pass
+
+
+def dim_filter_audio(edited_wave_file_list):
+    pass
+
 
 def edit_wave_file():
     # get user input (1-7)
     wave_file_list = wave_helper.load_wave(get_file())
-    print(wave_file_list)
+    edited_wave_file_list = copy.deepcopy(wave_file_list)
+    # print(wave_file_list)
     print_menu()
-    user_choice = input("")
+    user_choice = input("enter your choice: ")
+    if user_choice == 1:
+        edited_wave_file_list = reverse_audio(edited_wave_file_list)
+         
+    if user_choice == 2:
+        edited_wave_file_list = negative_audio(edited_wave_file_list)
+              
+    if user_choice == 3:
+        edited_wave_file_list = accelerate_audio(edited_wave_file_list)
+          
+    if user_choice == 4:
+        edited_wave_file_list = slow_down_audio(edited_wave_file_list)
+        
+    if user_choice == 5:
+        edited_wave_file_list = volume_up_audio(edited_wave_file_list)
+        
+    if user_choice == 6:
+        edited_wave_file =  volume_down_audio(edited_wave_file_list)
+    
+    if user_choice == 7:
+        edited_wave_file = dim_filter_audio(edited_wave_file_list)
+
+
 
 
     # 1. reverse
